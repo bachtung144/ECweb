182a5a87ce0507cb0b68815bcc41941acdf2055a
bachtung144 <bachtung144007@gmail.com>
2021-07-28 14:52:35 +0700

    Video 2: Create Shop, Cart And Checkout Page

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',\App\Http\Livewire\HomeComponent::class);
+Route::get('/shop',\App\Http\Livewire\ShopComponent::class);
+Route::get('/cart',\App\Http\Livewire\CartComponent::class);
+Route::get('/checkout',\App\Http\Livewire\CheckoutComponent::class);
 
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
