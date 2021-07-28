5fbeaea55f9b47a6e34c1a26ec360faa0a4e3d0c
bachtung144 <bachtung144007@gmail.com>
2021-07-28 15:56:58 +0700

    Video 3: Admin and User Authentication

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,15 @@ Route::get('/cart',\App\Http\Livewire\CartComponent::class);
 Route::get('/checkout',\App\Http\Livewire\CheckoutComponent::class);
 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//    return view('dashboard');
+//})->name('dashboard');
+
+//For User or Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function (){
+    Route::get('/user/dashboard',\App\Http\Livewire\User\UserDashboardComponent::class)->name('user.dashboard');
+});
+//For Admin
+Route::middleware(['auth:sanctum', 'verified','authadmin'])->group(function (){
+    Route::get('/admin/dashboard',\App\Http\Livewire\Admin\AdminDashboardComponent::class)->name('admin.dashboard');
+});
